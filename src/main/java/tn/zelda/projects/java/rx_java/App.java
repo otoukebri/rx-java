edff0cc19a61b9ee5bed071da6fa854fd570a87e
otoukebri <oussema.toukebri@sofrecom.com>
2018-08-28 15:05:51 +0200

    init example rx-java

---
+++ src/main/java/tn/zelda/projects/java/rx_java/App.java
@@ -0,0 +1,25 @@
+package tn.zelda.projects.java.rx_java;
+
+
+import io.reactivex.Flowable;
+import io.reactivex.functions.Consumer;
+
+/**
+ * Hello world!
+ *
+ */
+// https://examples.javacodegeeks.com/core-java/reactive-java-rxjava-tutorial-introduction/
+public class App {
+//	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+//		Flowable.just("Hello world").subscribe(new Consumer() {
+//
+//			@Override
+//			public void accept(String t) throws Exception {
+//				System.out.println(t);
+//			}
+//		});
+//		
+//		Flowable.just("Hello world").subscribe(System.out::println);
+//	}
+}
